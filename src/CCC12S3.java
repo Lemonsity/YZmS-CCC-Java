b5ddb78de9a7676c2743cbc7f695bb459e2fee8f
Lemonsity <Lemonsity@lemon.com>
2018-11-19 23:02:00 -0500

    Finish commenting CCC12S3

--- src/CCC12S3.java
+++ src/CCC12S3.java
@@ -10,10 +10,8 @@ import java.util.*;
  * Purpose: Solving 2012 CCC S3
  *
  ***********************************************************************
- *  Status: Work in progress
+ *  Status: Completed
  *
- *  - Took in user's input
- *  - figuring out the logic
  */
 public class CCC12S3 {
     public static void main(String[] args) throws IOException{
@@ -24,8 +22,12 @@ public class CCC12S3 {
             int input = Integer.parseInt(br.readLine());
             sensorCount[input]++;
         }
+
+        // This array will store the frequency of reads from least frequent to most frequent
+        // We only focus on index 998, 999, and 1000
         int[] sorted = sensorCount.clone();
         Arrays.sort(sorted);
+
         int cases; // If there are more than one most frequent, or there are mosre than one second most frequent, or neither
         if (sorted[1000] == sorted[999])
             cases = 1; // more than one most frequent read
@@ -33,44 +35,52 @@ public class CCC12S3 {
             cases = 2; // one most frequent read and 2 or more second most frequent read
         else
             cases = 3; // most frequent read and second most frequent read both only have one
-        int most = 0;
-        int secondMost = 0;
+        int most = 0; // The most frequent
+        int secondMost = 0; // The second most frequent
         if (cases == 1){
-            for (int i = 1; i <= 1000; i++){
+            for (int i = 1; i <= 1000; i++){ // Start at the front of the array that contain the frequency of read
                 if (sensorCount[i] == sorted[1000]){
-                    most = i;
+                    most = i; // First occurrence of the most frequent read
                     break;
                 }
             }
-            for (int i = 1000; i >= 1; i--){
+            for (int i = 1000; i >= 1; i--){ // Start at the back of array that contain the frequency of read
                 if (sensorCount[i] == sorted[1000]){
-                    secondMost = i;
+                    secondMost = i; // Last occurrence of the most frequent read
                     break;
                 }
             }
+            // Because we start at back and front separately, thus we know the difference is the highest there can be
+            // [] [] [] [] [] [] [] [] ...................... [] [] [] [] [] [] [] []
+            // ------->        The difference is for sure largest          <---------
         }
         else if (cases == 2){
-            for (int i = 1; i <= 1000; i++){
+            for (int i = 1; i <= 1000; i++){ // Find the most frequent read
                 if (sensorCount[i] == sorted[1000]){
                     most = i;
                     break;
                 }
             }
-            for (int i = 1; i <= 1000; i++){
+            for (int i = 1; i <= 1000; i++){ // Go from the front of array, find first occurrence of the second most frequent read
                 if (sensorCount[i] == sorted[999]){
                     secondMost = i;
                     break;
                 }
             }
-            for (int i = 1000; i >= 1; i--){
+            for (int i = 1000; i >= 1; i--){ // Go from back of the array, find the first occurrence of the second most frequent read
                 if (sensorCount[i] == sorted[999]){
+                    /*
+                     *  Because we went from the front and back and always find the first occurrence,
+                     *  thus we went on the extreme side of the spectrum
+                     *  and which ever one is the furthest away from the most frequent will also have the largest difference
+                     */
                     if (Math.abs(i - most) > Math.abs(secondMost - most))
                         secondMost = i;
                     break;
                 }
             }
         }
-        else{
+        else { // Just find the first and second most frequent reads
             for (int i = 1; i <= 1000; i++){
                 if (sensorCount[i] == sorted[1000]){
                     most = i;
@@ -84,6 +94,6 @@ public class CCC12S3 {
                 }
             }
         }
-        System.out.print(Math.abs(most - secondMost) + "\n");
+        System.out.print(Math.abs(most - secondMost) + "\n"); // Print out the absolute difference of the most and second most frequent
     }
 }
