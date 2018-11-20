c8c52fcca63f3a3b4b76a2c2220da8bced955ab6
Lemonsity <Lemonsity@lemon.com>
2018-11-19 23:13:10 -0500

    t

--- src/CCC12S3.java
+++ src/CCC12S3.java
@@ -12,6 +12,7 @@ import java.util.*;
  ***********************************************************************
  *  Status: Completed
  *
+ * tmep
  */
 public class CCC12S3 {
     public static void main(String[] args) throws IOException{
