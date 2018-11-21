49776c2fc66186028cc4af7884e55017f44b5850
Lemonsity <Lemonsity@lemon.com>
2018-11-20 23:42:20 -0500

    Adding to CCC04S3

--- src/CCC04S3.java
+++ src/CCC04S3.java
@@ -1,60 +1,44 @@
 import java.io.*;
 import java.util.HashMap;
-import java.util.Scanner;
-
+import java.util.Map;
+/**********************************************************************
+ * Project Name: CCC04S3
+ *
+ * Author: YZmS Lemonsity
+ *
+ * Date: Nov 20, 2018
+ *
+ * Purpose: Solving 2004 CCC S3
+ *
+ ***********************************************************************
+ *  Status: Work in Progress
+ *
+ */
 public class CCC04S3 {
-    public static HashMap<String, String> table = new HashMap<>();
+    public static Map<String, String> map = new HashMap<>();
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Scanner sc = new Scanner(System.in);
-        for (int row = 65 ; row < 75; row++){
-            for (int column = 1; column <= 9; column++){
-                String key = String.valueOf((char)row) + String.valueOf(column);
-                table.put(key, sc.next());
+        for (char character = 'A'; character <= 'J'; character++){
+            String[] temp = br.readLine().split(" ");
+            for (int i = 1; i <= 9; i++){
+                map.put(String.valueOf(character) + String.valueOf(i), temp[i - 1]);
             }
         }
-        for (char row = 'A' ; row <= 'J'; row++){
-            for (int column = 1; column <= 9; column++){
-                String key = String.valueOf(row) + String.valueOf(column);
-                String sumString = calculate(key);
-                int sumInt = 0;
-                String sumFinal = "";
-                String[] adders = sumString.split(" ");
-                for (String adder : adders){
-                    if (adder.equalsIgnoreCase("*")) {
-                        sumFinal = "*";
-                    }
-                    else{
-                        sumInt += Integer.parseInt(adder);
-                    }
+        for (char character = 'A'; character <= 'J'; character++){
+            for (int i = 1; i <= 9; i++){
+                if (!isInteger(map.get(String.valueOf(character) + String.valueOf(i)))){
+
                 }
-                if (sumFinal.equalsIgnoreCase("*"))
-                    sumFinal = String.valueOf(sumInt);
-                table.put(key, sumFinal);
             }
         }
     }
-    public static String calculate(String key){
-        String sum = "";
-        if (integer(table.get(key)))
-            sum = table.get(key);
-        else {
-            String[] adders = table.get(key).split("\\+");
-            for (String adder : adders){
-                if (adder.equalsIgnoreCase(key))
-                    sum += "* ";
-                else
-                    sum += calculate(adder);
-            }
-        }
-        return sum;
-    }
-    public static boolean integer(String value){
+    public static boolean isInteger(String value){
         boolean check = false;
         try{
             Integer.parseInt(value);
             check = true;
-        } catch (NumberFormatException ex){
+        } catch (Exception e){
+
         }
         return check;
     }
