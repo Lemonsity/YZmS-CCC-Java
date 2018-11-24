52ccc328ea7093b07153662943e5352522a0b6d2
Lemonsity <Lemonsity@lemon.com>
2018-11-24 02:51:26 -0500

    Finish CCC05S1

--- src/CCC05S1.java
+++ src/CCC05S1.java
@@ -1,29 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+/***********************************************************************
+ * Project Name: CCC05S1
+ *
+ * Author: YZmS Lemonsity
+ *
+ * Date: Nov 24, 2018
+ *
+ * Purpose: Solving 2005 CCC S1
+ *
+ ***********************************************************************
+ *  Status: Completed
+ *
+ */
 public class CCC05S1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int amount = Integer.parseInt(br.readLine());
+        int amount = Integer.parseInt(br.readLine()); // amount of inputs
         for (int i = 0; i < amount; i++) {
-            String input = br.readLine();
-            int[] finalOutput = new int[10];
-            int counter = 0;
-            int index = 0;
-            while (counter < 10) {
-                if (input.charAt(index) != '-') {
+            String input = br.readLine(); // take in value as a string
+            int[] finalOutput = new int[10]; // set an array which holds the value after process
+            int counter = 0; // amount of number found
+            int index = 0; // index of string checking
+            while (counter < 10) { // when found less than 10 numbers
+                if (input.charAt(index) != '-') { // check if the string index at is a -, if not, then turn it into a number
                     int temp = (int) input.charAt(index);
-                    if (temp >= 48 && temp <= 57) {
+                    if (temp >= 48 && temp <= 57) { // if the char is already number
                         finalOutput[counter] = temp - 48;
                     } else {
-                        finalOutput[counter] = letter(temp);
+                        finalOutput[counter] = letter(temp); // if the char is not number but an uppercase
                     }
-                    counter++;
+                    counter++; // add to to amount of number found
                 }
-                index++;
+                index++; // move onto next char
             }
-            for (int j = 0; j < 10; j++){
+            for (int j = 0; j < 10; j++){ //output
                 System.out.print(finalOutput[j]);
                 if (j == 2 || j == 5)
                     System.out.print('-');
@@ -31,8 +43,7 @@ public class CCC05S1 {
             System.out.print("\n");
         }
     }
-
-    public static int letter(int value) {
+    private static int letter(int value) { // for each char, return different integer
         if (value >= 65 && value <= 67)
             return 2;
         else if (value >= 68 && value <= 70)
@@ -43,9 +54,9 @@ public class CCC05S1 {
             return 5;
         else if (value >= 77 && value <= 79)
             return 6;
-        else if (value >= 80 && value <= 82)
+        else if (value >= 80 && value <= 83)
             return 7;
-        else if (value >= 83 && value <= 85)
+        else if (value >= 84 && value <= 86)
             return 8;
         return 9;
     }
