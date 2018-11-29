a3b29a44ee46f527715bfefcbc7d55360a936900
Lemonsity <youzhang.mark.sun@gmail.com>
2018-11-28 21:58:58 -0500

    Adding finished CCC06S1, CCC06S2

--- src/CCC06S2.java
+++ src/CCC06S2.java
@@ -23,7 +23,7 @@ public class CCC06S2 {
         Map<Character, Character> map = new HashMap<>(); // map, Key is the real text, Value is the cipher text
         Map<Character, Character> mapReverse = new HashMap<>(); // the first map, but now each available cipher is the Key, and the real text is the Value
         for (int i = 0; i < plain.length(); i++) {
-            if (!map.containsKey(plain.indexOf(i))) // if first map still don't have the real text at index i in keyset
+            if (!map.containsKey(plain.charAt(i))) // if first map still don't have the real text at index i in keyset
                 map.put(plain.charAt(i), cipher1.charAt(i)); // add the new occurring real text to the keyset, along with the cipher text that matches
         }
         for (Character key : map.keySet()){
