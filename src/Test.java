4d5a75869a633392a6aa171607ef6626bab9856f
Lemonsity <Lemonsity@lemon.com>
2018-11-29 14:46:37 -0500

    Test

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+// Test
+// Test2
 public class Test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
