5210bb04cab5412b698b41b56f3a2f77801daef6
Lemonsity <youzhang.mark.sun@gmail.com>
2018-11-29 23:14:14 -0500

    Edit CCC06S3

--- src/CCC06S3.java
+++ src/CCC06S3.java
@@ -1,70 +1,52 @@
 import java.io.*;
+
 public class CCC06S3 {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int counter = 0;
         double[] friends = new double[4];
         String[] temp = br.readLine().split(" ");
         for (int i = 0; i < 4; i++)
-            friends[i] = Double.parseDouble(temp[i]);
-        if (friends[2] != friends[0]) {
-            double slope = (friends[3] - friends[1]) / (friends[2] - friends[0]);
-            double yInter = friends[1] - friends[0] * slope;
-            int numBuilding = Integer.parseInt(br.readLine());
-            int[][] buildings = new int[numBuilding][];
-            for (int i = 0; i < numBuilding; i++) {
-                temp = br.readLine().split(" ");
-                buildings[i] = new int[temp.length];
-                for (int j = 0; j < temp.length; j++) {
-                    buildings[i][j] = Integer.parseInt(temp[j]);
-                }
-                if (slope * buildings[i][1] + yInter == buildings[i][2]) { // if first corner land on the telephone line
+            friends[i] = Double.parseDouble(temp[i]) * 1.0;
+        double slope = (friends[3] - friends[1]) * 1.0 / (friends[2] - friends[0]); // the slope of telephone line
+        double yInter = friends[1] - friends[0] * slope; // Y intersect of the line representing telephone
+        int buildingNum = Integer.parseInt(br.readLine());
+        for (int i = 0; i < buildingNum; i++) {
+            temp = br.readLine().split(" ");
+            double[] building = new double[temp.length];
+            for (int j = 0; j < building.length; j++) {
+                building[j] = Double.parseDouble(temp[j]);
+            }
+            if (building[1] * slope + yInter == building[2]){
+                if (building[1] <= Math.max(friends[0], friends[2]) && building[1] >= Math.min(friends[0], friends[2])) {
+                    System.out.print("Test1");
                     counter++;
                     continue;
-                }
-                boolean firstCorner;
-                if (slope * buildings[i][1] + yInter > buildings[i][2]) // If the first corner lands above the telephone line
-                    firstCorner = true;
-                else // if the first corner lands below the telephone line
-                    firstCorner = false;
-                for (int j = 2; j <= buildings[i][0]; j++) {
-                    if (slope * buildings[i][j * 2 - 1] + yInter == buildings[i][j * 2]) { // if a corner is on the telephone line
-                        counter++;
-                        break;
-                    } else if ((slope * buildings[i][j * 2 - 1] + yInter > buildings[i][j * 2]) != firstCorner) { // if a corner is on the opposite side as the first corner
-                        counter++;
-                        break;
-                    }
                 }
             }
-        }
-        else {
-            int numBuilding = Integer.parseInt(br.readLine());
-            int[][] buildings = new int[numBuilding][];
-            for (int i = 0; i < numBuilding; i++) {
-                temp = br.readLine().split(" ");
-                buildings[i] = new int[temp.length];
-                for (int j = 0; j < temp.length; j++) {
-                    buildings[i][j] = Integer.parseInt(temp[j]);
+            double firstPointX = building[1] * 1.0;
+            double firstPointY = building[2] * 1.0;
+            for (int j = 2; j <= building[0]; j++) {
+                double secondPointX = building[j * 2 - 1] * 1.0;
+                double secondPointY = building[j * 2] * 1.0;
+                if (secondPointX * slope + yInter == secondPointY){
+                    if (building[1] <= Math.max(friends[0], friends[2]) && building[1] >= Math.min(friends[0], friends[2])) {
+                        System.out.print("Test2");
+                        counter++;
+                        break;
+                    }
                 }
-                if (buildings[i][1] == friends[0]){
+                double slopeTwo = (secondPointY - firstPointY) * 1.0 / (secondPointX - firstPointX);
+                double yInterTwo = firstPointY - firstPointX * slopeTwo;
+                double intersectX = (yInterTwo - yInter) / (slope - slopeTwo);
+                if (intersectX <= Math.max(friends[0], friends[2]) && intersectX >= Math.min(friends[0], friends[2])){
+                    System.out.print("Test3");
                     counter++;
-                    continue;
+                    break;
                 }
-                boolean first;
-                if (buildings[i][1] < friends[0])
-                    first = true;
-                else
-                    first = false;
-                for (int j = 2; j <= buildings[i][0]; j++) {
-                    if (buildings[i][j * 2 - 1] == friends[0]){
-                        counter++;
-                        break;
-                    }
-                    else if ((buildings[i][1] < friends[0]) != first){
-                        counter++;
-                        break;
-                    }
+                else {
+                    firstPointX = secondPointX;
+                    firstPointY = secondPointY;
                 }
             }
         }
--- src/Test.java
+++ src/Test.java
@@ -6,10 +6,9 @@ import java.io.InputStreamReader;
 public class Test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] temp = br.readLine().toCharArray();
-        for (int i = temp.length - 1; i >= 0; i--){
-            System.out.print(temp[i]);
-        }
+        int x = 8;
+        int y = 5;
+        System.out.print(x / y);
     }
 }
 
