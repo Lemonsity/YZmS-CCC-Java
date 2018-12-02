e60886a08c4d105093c3913d65abed2e3f7ac203
Lemonsity <Lemonsity@lemon.com>
2018-12-02 16:04:29 -0500

    Add CCC05S5

---
+++ src/CCC05S5.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class CCC05S5 {
+    public static void main(String[] args) {
+
+    }
+}
+class Node{
+    Node left;
+    Node right;
+    int value;
+    public Node(int value) {
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}
